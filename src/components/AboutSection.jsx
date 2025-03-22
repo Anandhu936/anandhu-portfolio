92f258b6015478d9fbd0698af9428b015a195420
anandhu <anandhunair777@gmail.com>
2025-03-22 13:05:40 +0530

    styles changed

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutSection = () => {
   return (
     <div id="About" className=" flex justify-center ">
-      <div className="w-full xl:w-[1200px] pt-[40px] lg:pt-[80px] pb-[60px] lg:pb-[100px] mx-8  rounded-lg">
+      <div className="w-full xl:w-[1200px] pt-[40px] lg:pt-[80px] pb-[20px] lg:pb-[100px] mx-8  rounded-lg">
         <h1 className="text-4xl flex justify-center text-blue-500 underline decoration-blue-800 underline-offset-8">About</h1>
         <div className=" lg:grid lg:grid-cols-2 lg:gap-0 gap-6 mt-0 lg:mt-[80px] p-5">
           <div className="flex justify-center">
@@ -16,7 +16,7 @@ const AboutSection = () => {
           <div className="flex flex-col justify-center">
           <h2 className="mb-2 lg:mb-4 text-2xl font-medium text-white text-center lg:text-left mt-2 lg:mt-0">I'm Anandhu</h2>
 
-            <p className="text-xl font-light text-white text-center lg:text-left">
+            <p className="text-md lg:text-xl font-light text-white text-center lg:text-left">
             Im a Frontend Developer with expertise in JavaScript, CSS, HTML, Next.js, React.js, Tailwind CSS, and Bootstrap. Your skill set enables you to build modern, responsive, and interactive web applications with a strong focus on user experience and performance. You are proficient in developing scalable UI components, optimizing web interfaces, and ensuring cross-browser compatibility. Your experience with frameworks like Next.js and React.js allows you to create dynamic, SEO-friendly, and efficient applications. With your knowledge of styling frameworks like Tailwind CSS and Bootstrap, you can design visually appealing and maintainable user interfaces.
             </p>
           </div>
--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -10,25 +10,25 @@ const ContactSection = () => {
         Contact Me
       </h1>
       <div className=" flex justify-center ">
-        <div className="flex justify-center gap-8 mt-8 border-2 border-yellow-300 p-4 lg:p-10 rounded-xl max-w-fit  mx-4">
-          <div className="flex justify-center items-center gap-4">
+        <div className="flex justify-center gap-4 lg:gap-8 mt-8 border-2 border-yellow-300 p-2 lg:p-10 rounded-xl max-w-fit  mx-4">
+          <div className="flex justify-center items-center gap-2 lg:gap-4">
             <div className="flex border-2 border-white p-2 rounded-full bg-black">
               <box-icon name="envelope" color="white"></box-icon>
             </div>
             <a
               href="mailto:examplemail@gmail.com"
-             className="!text-white hover:!text-green-500 text-[24px] lg:text-[32px]"
+             className="!text-white hover:!text-green-500 text-[14px] lg:text-[32px]"
             >
               Example@gmail.com
             </a>
           </div>
-          <div className="flex justify-center items-center gap-4">
+          <div className="flex justify-center items-center gap-2 lg:gap-4">
             <div className="flex border-2 border-white p-2 rounded-full bg-black">
               <box-icon name="linkedin" type="logo" color="white "></box-icon>
             </div>
             <a
               href="#"
-              className="!text-white hover:!text-green-500 text-[24px] lg:text-[32px]"
+              className="!text-white hover:!text-green-500 text-[14px] lg:text-[32px]"
             >
               Linkedin
             </a>
