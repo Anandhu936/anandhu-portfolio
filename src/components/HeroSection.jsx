9e7f2195587524de64283228bf7403ff74f24344
anandhu <anandhunair777@gmail.com>
2025-03-24 12:35:30 +0530

    hero-img fixed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
           <div className=" lg:grid lg:grid-cols-2 lg:gap-0 gap-6 ">
             <div className="flex justify-center  ">
               <img
-                className="rounded-full w-60 lg:w-[110] lg-60 lg:h-[110] lg:opacity-[80] border-2 border-[#fff] shadow-[0_0_20px_#FFF] mt-0 lg:mt-[20]"
+                className="rounded-full w-60 lg:w-110 lg-60 lg:h-110 lg:opacity-80 border-2 border-[#fff] shadow-[0_0_20px_#FFF] mt-0 lg:mt-20"
                 src="https://anandhu936.github.io/anandhu-portfolio/images/my-image-new.png"
                 alt="my image"
               />
