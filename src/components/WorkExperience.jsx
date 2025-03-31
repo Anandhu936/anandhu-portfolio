17067d672f65704c07fdd6e8ae98e7d8a1d06ac3
anandhu <anandhunair777@gmail.com>
2025-03-31 14:44:59 +0530

    added new work experience

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const WorkExperience = () => {
   return (
-    <div id="Experience" className=" w-full px-[10%]">
+    <div id="Experience" className=" w-full px-[10%] lg:px-[20%]">
       <h1 className="flex justify-center text-3xl lg:text-5xl text-white mb-[40px] lg:mb-[60px] underline underline-offset-8">
         Work Experience
       </h1>
-      <div className="grid grid-cols lg:grid-cols-3 gap-14">
-        <div className="border-2 border-[#e9d713] p-10 rounded-2xl shadow-[0_0_20px_#e9d713]">
+      <div className="grid grid-cols lg:grid-cols-2 gap-10 lg:gap-35">
+        <div className="border-2 border-[#e9d713] p-2 lg:p-5 rounded-2xl shadow-[0_0_20px_#e9d713]">
           <img
-            className="h-[400px] w-[100%] rounded-xl"
+            className="h-[250px] lg:h-[400px] w-[100%] rounded-xl"
             src="https://anandhu936.github.io/anandhu-portfolio/images/coupon-generator.png"
             alt=""
           />
@@ -26,9 +26,9 @@ const WorkExperience = () => {
             </a>
           </div>
         </div>
-        <div className="border-2 border-[#e9d713] p-10 rounded-2xl shadow-[0_0_20px_#e9d713]">
+        <div className="border-2 border-[#e9d713] p-2 lg:p-5 rounded-2xl shadow-[0_0_20px_#e9d713]">
           <img
-            className="h-[400px] w-[100%] rounded-xl"
+            className="h-[250px] lg:h-[400px] w-[100%] rounded-xl"
             src="https://anandhu936.github.io/anandhu-portfolio/images/buying-power-calculator.png"
             alt=""
           />
@@ -45,9 +45,9 @@ const WorkExperience = () => {
             </a>
           </div>
         </div>
-        <div className="border-2 border-[#e9d713] p-10 rounded-2xl shadow-[0_0_20px_#e9d713]">
+        <div className="border-2 border-[#e9d713] p-2 lg:p-5 rounded-2xl shadow-[0_0_20px_#e9d713]">
           <img
-            className="h-[400px] w-[100%] rounded-xl"
+            className="h-[250px] lg:h-[400px] w-[100%] rounded-xl"
             src="https://anandhu936.github.io/anandhu-portfolio/images/saas-price-calculator.png"
             alt=""
           />
@@ -60,6 +60,25 @@ const WorkExperience = () => {
               {" "}
               <p className="text-center text-sm lg:text-2xl text-white hover:text-green-500 underline underline-offset-4">
                 Saas Price Calculator
+              </p>
+            </a>
+          </div>
+        </div>
+        <div className="border-2 border-[#e9d713] p-2 lg:p-5 rounded-2xl shadow-[0_0_20px_#e9d713]">
+          <img
+            className="h-[250px] lg:h-[400px] w-[100%] rounded-xl"
+            src="https://anandhu936.github.io/anandhu-portfolio/images/Yt-thumbnails.png"
+            alt=""
+          />
+          <div className="mt-4">
+            <a
+              href="https://www.lightgalleryjs.com/download-youtube-thumbnails/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              <p className="text-center text-sm lg:text-2xl text-white hover:text-green-500 underline underline-offset-4">
+                Youtube thumbnail downloader
               </p>
             </a>
           </div>
