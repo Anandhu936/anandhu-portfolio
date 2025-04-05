713b7984c399f10c2b6934d5273cc04083b21ac2
anandhu <anandhunair777@gmail.com>
2025-04-05 21:06:45 +0530

    gym-project-added

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -8,7 +8,7 @@ const MyProject = () => {
           {" "}
           Projects
         </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
           <div className="border-1 border-white p-2 lg:p-5 rounded-2xl ">
             <div className="h-[150px] md:h-[200px] w-[100%] overflow-hidden">
               <img
@@ -100,6 +100,38 @@ const MyProject = () => {
               </a>
               <a
                 href="https://anandhu936.github.io/military-time-converter/" target="_blank"
+              rel="noopener noreferrer"
+                className="border-2 border-black rounded-full px-5 py-2 cursor-pointer text-[12px] lg:text-[16px] text-white hover:text-green-500 transition-all"
+              >
+                Live Demo
+              </a>
+            </div>
+          </div>
+          <div className="border-1 border-white p-2 lg:p-5 rounded-2xl ">
+            <div className="h-[150px] md:h-[200px] w-[100%] overflow-hidden">
+              <img
+                className="w-full h-full  rounded-xl"
+                src="https://anandhu936.github.io/anandhu-portfolio/images/gym-website.png"
+                alt="military-time-converter"
+              />
+            </div>
+            <div className="mt-1 lg:mt-3">
+            
+                <p className="text-center text-[16px]  lg:text-[24px] text-white  ">
+                  Gym-website-Demo
+                </p>
+             
+            </div>
+            <div className='mt-2 lg:mt-5 flex justify-center gap-3 mb-2 lg:mb-0'>
+              <a
+                href="https://github.com/Anandhu936/gym-website"  target="_blank"
+              rel="noopener noreferrer"
+                className="border-2 border-black rounded-full px-5 py-2 cursor-pointer text-[12px] lg:text-[16px] text-white hover:text-green-500 transition-all"
+              >
+                GitHub
+              </a>
+              <a
+                href="https://anandhu936.github.io/gym-website/" target="_blank"
               rel="noopener noreferrer"
                 className="border-2 border-black rounded-full px-5 py-2 cursor-pointer text-[12px] lg:text-[16px] text-white hover:text-green-500 transition-all"
               >
